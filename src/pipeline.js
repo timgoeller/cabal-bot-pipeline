886faca47231297d1316283d5da19a7024b0e0db
Tim Göller <tim.goeller@posteo.de>
2020-08-13 13:44:08 +0200

    add option to also consider unprefixed messages

--- src/pipeline.js
+++ src/pipeline.js
@@ -1,20 +1,28 @@
 var CabalBotExpression = require('./expression')
 
-function pipeline (bot) {
-  const pipeline = new Pipeline(bot)
+function pipeline (bot, prefixRequired = true) {
+  const pipeline = new Pipeline(bot, prefixRequired)
   return pipeline.expression
 }
 
 class Pipeline {
-  constructor (bot) {
+  constructor (bot, prefixRequired) {
     this.expression = new CabalBotExpression()
+    this.expression.prefixRequired = prefixRequired
     this.bot = bot
     this._registerEvents()
   }
 
   _registerEvents () {
     this.bot.on('new-command', (envelope, cabalDetails) => {
-      this.expression._run(envelope, cabalDetails)
+      if (this.expression.prefixRequired) {
+        this.expression._run(envelope, cabalDetails)
+      }
+    })
+    this.bot.on('new-message', (envelope, cabalDetails) => {
+      if (!this.expression.prefixRequired) {
+        this.expression._run(envelope, cabalDetails)
+      }
     })
   }
 }
